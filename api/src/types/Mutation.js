bb9f2bb2121da5cb0d693bfe6c23ebe0ba86d820
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-28 16:47:35 -0500

    created get post mutation

--- api/src/types/Mutation.js
+++ api/src/types/Mutation.js
@@ -4,33 +4,40 @@ const {
   GraphQLString,
   GraphQLID
 } = require("graphql");
+const { mutationWithClientMutationId } = require("graphql-relay");
 
-const Post = require("./Post");
+const { Post } = require("./Post");
 const PostModel = require("../models/PostModel");
+
+const CreatePostMutation = mutationWithClientMutationId({
+  name: "CreatePost",
+  inputFields: {
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) }
+  },
+  outputFields: {
+    post: {
+      type: Post
+    }
+  },
+
+  mutateAndGetPayload: args => {
+    return new Promise((resolve, reject) => {
+      PostModel.createPost({
+        title: args.title,
+        content: args.content
+      })
+        .then(post => resolve({ post }))
+        .catch(reject);
+    });
+  }
+});
 
 const Mutation = new GraphQLObjectType({
   name: "Mutation",
   description: "Mutation interface",
   fields: {
-    createPost: {
-      type: Post,
-      args: {
-        title: {
-          type: GraphQLString,
-          description: "Title of post"
-        },
-        content: {
-          type: GraphQLString,
-          description: "Content of post"
-        }
-      },
-      resolve: (_, args) => {
-        return PostModel.createPost({
-          title: args.title,
-          content: args.content
-        });
-      }
-    }
+    createPost: CreatePostMutation
   }
 });
 
