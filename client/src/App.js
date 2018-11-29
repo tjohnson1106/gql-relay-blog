870edb4983f89ce745015f7d80b8f0fc3a534ffd
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-29 15:10:28 -0500

    implemented query rendered in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+import { QueryRenderer, graphql } from "react-relay";
+import { environment } from "./relay/Environment";
 
 import ListPage from "./components/ListPage";
 
+const AppAllPostQuery = graphql`
+  query AppAllPostQuery {
+    viewer {
+      ...ListPage_viewer
+    }
+  }
+`;
+
 class App extends Component {
   render() {
-    return <ListPage />;
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={AppAllPostQuery}
+        render={({ error, props }) => {
+          if (error) {
+            return <div>{error.message}</div>;
+          } else if (props) {
+            return <ListPage viewer={props.viewer} />;
+          }
+          return <div>Loading</div>;
+        }}
+      />
+    );
   }
 }
 
