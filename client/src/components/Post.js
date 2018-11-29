1cce2ac65fdbd43254f4b264d3bdef77d6c64454
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-29 13:10:41 -0500

    created post component

---
+++ client/src/components/Post.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import Modal from "react-modal";
+
+class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+  render() {
+    return (
+      <div
+        style={styles.postWrapper}
+        onClick={() =>
+          this.setState({
+            isOpen: !this.state.isOpen
+          })
+        }
+      >
+        <h3>{this.props.post.title}</h3>
+        <Modal isOpen={this.state.isOpen}>
+          <h3>{this.props.post.title}</h3>
+          <div>{this.props.post.content}</div>
+          <button
+            style={styles.closeButton}
+            onClick={() =>
+              this.setState({
+                isOpen: false
+              })
+            }
+          >
+            x
+          </button>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  postWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    border: "1px solid #ccc",
+    margin: 10,
+    cursor: "pointer"
+  },
+  closeButton: {
+    position: "absolute",
+    right: 12,
+    top: 10,
+    border: 0,
+    background: "white",
+    fontSize: 25,
+    color: "gray"
+  }
+};
+
+export default Post;
--- client/src/data/mockPostData.js
+++ client/src/data/mockPostData.js
@@ -4,8 +4,9 @@ export const mockPostData = [
       id: 1,
       title: "First Post Title",
       content: "First Post Content"
-    },
-
+    }
+  },
+  {
     node: {
       id: 2,
       title: "Second Post Title",
