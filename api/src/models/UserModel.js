e811c4e58142d491246185eecb7d78b405c139be
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-30 17:29:18 -0500

    created user model

---
+++ api/src/models/UserModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const db = require("../../config/key").mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("mongodb connected"))
+  .catch(err => console.log("mongodb error", err, "mongodb error"));
+
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  fullname: String
+});
+
+var UserModal = mongoose.model("User", userSchema);
+
+module.exports = {
+  getUser: (id, username) => {
+    return UserModal.findOne({
+      $or: [{ _id, id }, { username: username }]
+    });
+  },
+  loginUser: loginUserInput => {
+    const { username, password } = loginUserInput;
+    return UserModal.findOne({
+      $and: [{ username: username }, { password, password }]
+    });
+  },
+
+  createUser: user => {
+    return UserModal(user).save();
+  }
+};
