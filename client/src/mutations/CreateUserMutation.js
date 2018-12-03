c26a2a15f2706b5c742edbe589c561a7234772a2
Thomas Johnson <tjohnson1106@gmail.com>
2018-12-03 14:46:20 -0500

    created create user mutation

---
+++ client/src/mutations/CreateUserMutation.js
@@ -0,0 +1,32 @@
+import { commitMutation, graphql } from "react-relay";
+import { environment } from "../relay/Environment";
+
+const mutation = graphql`
+  mutation CreateUserMutation($createUserInput: CreateUserInput!) {
+    CreateUser(input: $createUserInput) {
+      user {
+        id
+      }
+    }
+  }
+`;
+
+export default (username, password, fullname, callback) => {
+  const variables = {
+    createUserInput: {
+      username,
+      password,
+      fullname,
+      clientMutationId: ""
+    }
+  };
+
+  commitMutation(environment, {
+    mutation,
+    variables,
+    onCompleted: response => {
+      callback(response.createUser.user);
+    },
+    onError: err => console.log("Error!: ", err, "Error!")
+  });
+};
