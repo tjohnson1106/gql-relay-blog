e24d6dec79b8cdcb152e7fd3a4d050fe4764a223
Thomas Johnson <tjohnson1106@gmail.com>
2018-12-03 14:36:48 -0500

    created login UI component

--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,14 @@ import { BrowserRouter, Switch } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import CreatePost from "./components/CreatePost";
-
-// TODO: 120120181249 connect client to api authentication
+import Login from "./components/Login";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <App path="/" exact={true} />
       <CreatePost path="/create-post" exact={true} />
+      <Login path="/login" exact={true} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
